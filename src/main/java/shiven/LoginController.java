6749767dc4749942296703a8d3344b88547d5352
GeorgePerakis <giorgosperakis100@gmail.com>
2024-11-16 15:03:34 +0200

    Added Login Screen and Refactored Code

--- src/main/java/shiven/LoginController.java
+++ src/main/java/shiven/LoginController.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class LoginController {
     UserDAO userDAO = new UserDAO();
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void switchToRegister() throws IOException {
+        App.setRoot("Register");
     }
 
     @FXML
---
+++ src/main/java/shiven/RegisterController.java
@@ -0,0 +1,13 @@
+package shiven;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class RegisterController {
+
+    @FXML
+    private void switchToLogin() throws IOException {
+        App.setRoot("Login");
+    }
+}
